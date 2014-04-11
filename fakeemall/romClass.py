74e07d6fd464ab70457937d198eea254479e801b
Akwa <pokemonfakeemall@gmail.com>
2014-04-11 20:11:56 +0200

    some corrects following pep8 guidelines

--- fakeemall/romClass.py
+++ fakeemall/romClass.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from constants import *
+from pokemonClass import Pokemon
 from re import match
 
 class Rom(object):
 
-    def __init__(self, path, outPath='fakeem.gbc'):
+    def __init__(self, path, outPath=None):
         """
         Input path and output path is set here.
         """
+        if outPath is None:
+            outPath = 'fakeem.gbc'
         self.outPath = outPath
         self.readRom(path)
         self.parseRom()
@@ -31,6 +34,7 @@ class Rom(object):
         self.readVersion()
         self.setPointers()
         self.loadData()
+        self.createClasses()
 
     def readVersion(self):
         """
@@ -38,81 +42,70 @@ class Rom(object):
         Establishes self.version attribute basing on content of it.
         """
         hdrStart = 0x134
-        hdrEnd = 0x13e
+        hdrEnd = 0x13E
         header = self.data[hdrStart:hdrEnd]
         self.version = {'POKEMON_GL': 'Gold',
                         'POKEMON_SL': 'Silver',
                         'PM_CRYSTAL': 'Crystal'
                         }.get(header)
 
-    def overwriteRom(self):
-        """
-        Overwrites 'self.data*' attrs with new randomized data,
-        assembles it back to 'self.data' and creates a new .gbc file.
-        """
-        self.updateDatas()
-        self.assemblyData()
-        with open(self.outPath, 'wb') as rom:
-            rom.write(self.data)
-
-    
     def setPointers(self):
         """
         Depending on version, selects particular pointers
         for particular data blocks.
         """
         # Evolution and moveset index bounds
-        evomovesStart = {'Gold' : 0x42790,
-                         'Silver' : 0x42790,
+        evomovesStart = {'Gold': 0x42790,
+                         'Silver': 0x42790,
                          'Crystal': 0x42584
                          }.get(self.version)
         evomovesEnd = 0x44000  # Reserving the tail of bank
         self.pntEvomoves = (evomovesStart, evomovesEnd)
 
         # Base stats index bounds
-        basestatsStart = {'Gold' : 0x51b0b,
-                          'Silver' : 0x51b0b,
+        basestatsStart = {'Gold': 0x51B0B,
+                          'Silver': 0x51B0B,
                           'Crystal': 0x51424
                           }.get(self.version)
         basestatsEnd = basestatsStart + maxPokemon * lenBasestat
         self.pntBasestats = (basestatsStart, basestatsEnd)
 
         # Moves data index bounds
-        movesStart = {'Gold' : 0x41afe,
-                      'Silver' : 0x41afe,
-                      'Crystal': 0x41afb
+        movesStart = {'Gold': 0x41AFE,
+                      'Silver': 0x41AFE,
+                      'Crystal': 0x41AFB
                       }.get(self.version)
         movesEnd = movesStart + maxMoves * lenMove
         self.pntMoves = (movesStart, movesEnd)
 
         # Palette data index bounds
-        palettesStart = {'Gold' : 0xad45,
-                         'Silver' : 0xad45,
-                         'Crystal': 0xad86
+        palettesStart = {'Gold': 0xAD45,
+                         'Silver': 0xAD45,
+                         'Crystal': 0xAD86
                          }.get(self.version)
         palettesEnd = palettesStart + maxPokemon * lenPalette
         self.pntPalettes = (palettesStart, palettesEnd)
 
         # Pokémon names data index bounds
-        namesStart = {'Gold' : 0x1b0b74,
-                      'Silver' : 0x1b0b74,
+        namesStart = {'Gold': 0x1B0B74,
+                      'Silver': 0x1B0B74,
                       'Crystal': 0x53384
                       }.get(self.version)
         namesEnd = namesStart + maxPokemon * lenName
         self.pntNames = (namesStart, namesEnd)
 
         # Move names data index bounds
-        movenamesStart = {'Gold' : 0x1b1574,
-                          'Silver' : 0x1b1574,
-                          'Crystal': 0x1c9f29
+        movenamesStart = {'Gold': 0x1B1574,
+                          'Silver': 0x1B1574,
+                          'Crystal': 0x1C9f29
                           }.get(self.version)
         movenamesEnd = movenamesStart + maxMoves * lenMovename
         self.pntMovenames = (movenamesStart, movenamesEnd)
 
         # Moves TM data index bounds
-        tmStart = {'Gold' : 0x11a66,
-                   'Silver' : 0x11a66,
-                   'Crystal': 0x1167a
+        tmStart = {'Gold': 0x11A66,
+                   'Silver': 0x11A66,
+                   'Crystal': 0x1167A
                    }.get(self.version)
         tmEnd = tmStart + maxTms * lenTm
         self.pntTms = (tmStart, tmEnd)
@@ -129,6 +122,24 @@ class Rom(object):
         self.dataMovenames = self.data.__getslice__(*self.pntMovenames)
         self.dataTms = self.data.__getslice__(*self.pntTms)
 
+    def createClasses(self):
+        """
+        Creates classes from the data blocks, from where
+        will be randomized.
+        """
+        self.pokemon = Pokemon()
+        self.pokemon.extractNames(self.dataNames)
+
+    def overwriteRom(self):
+        """
+        Overwrites 'self.data*' attrs with new randomized data,
+        assembles it back to 'self.data' and creates a new .gbc file.
+        """
+        self.updateDatas()
+        self.assemblyData()
+        with open(self.outPath, 'wb') as rom:
+            rom.write(self.data)
+
     def updateDatas(self):
         """
         The 'self.data*' attrs contain the "byte sequences" of data.
@@ -136,7 +147,7 @@ class Rom(object):
         the 'self.data*' attr with data converted to "byte sequence".
         """
         pass
-    
+
     def assemblyData(self):
         """
         This piece of code overwrites the 'self.data'
@@ -145,7 +156,7 @@ class Rom(object):
         """
         pntPat = 'pnt[A-Z]{1}[a-zA-Z]*'
         dataPat = 'data[A-Z]{1}[a-zA-Z]*'
-        
+
         # Grab all 'self.data*' and 'self.pnt*' attrs and sort them
         items = self.__dict__.iteritems
         allDatas = [v for k, v in sorted(items()) if match(dataPat, k)]
@@ -162,4 +173,3 @@ class Rom(object):
         else:
             newdata += self.data[j:]
         self.data = newdata
-

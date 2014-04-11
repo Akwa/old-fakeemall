f0aa8d3bdf3643c010b7a8f606b08824398b5013
Akwa <pokemonfakeemall@gmail.com>
2014-04-11 17:43:16 +0200

    initial commit

---
+++ fakeemall/constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+maxPokemon = 251
+maxMoves = 251
+maxTms = 61
+
+lenBasestat = 32
+lenMove = 7
+lenPalette = 8
+lenName = 10
+lenMovename = 10
+lenTm = 1

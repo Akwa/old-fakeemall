17a64431dd15e448e9b16aca8c68c1fada4ae20e
Akwa <pokemonfakeemall@gmail.com>
2014-04-12 20:32:50 +0200

    extractBasestats more readable

--- fakeemall/pokemonClass.py
+++ fakeemall/pokemonClass.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from constants import *
+from helpingFunctions import *
 import random
 
 class Pokemon(object):
@@ -36,59 +37,40 @@ class PokemonContainer(object):
         Some of these stats might be unneeded in randomization,
         but they are extracted anyway to help in future testing.
         """
-        hexToDec = lambda x: int(x, 16)
         for i in xrange(maxPokemon):
             j = i * lenBasestat
             # byteSeq is the sequence of 32 bytes
             byteSeq = data[j:j + lenBasestat]
-            # extract certain stats from certain locations.
+
             # *** Base stats ***
-            # cut out the stats and make them an int list
-            # eg. Celebi: map(ord, 'dddddd') = [100, ..., 100]
-            stats = map(ord, byteSeq[0x1:0x7])
-            self.pokemon[i].stats = stats
+            self.pokemon[i].stats = processData(byteSeq[0x1:0x7])
+
             # *** Types ***
-            # similarly, uses map to change str into int list
-            types = map(ord, byteSeq[0x7:0x9])
-            self.pokemon[i].types = types
+            self.pokemon[i].types = processData(byteSeq[0x7:0x9])
+
             # *** Catch rate ***
-            catchRate = ord(byteSeq[0x9])
-            self.pokemon[i].catchRate = catchRate
+            self.pokemon[i].catchRate = processData(byteSeq[0x9])
+
             # *** Exp yield ***
-            expYield = ord(byteSeq[0xa])
-            self.pokemon[i].expYield = expYield
+            self.pokemon[i].expYield = processData(byteSeq[0xa])
+
             # *** Wild held items ***
-            wildHeldItems = map(ord, byteSeq[0xb:0xd])
-            self.pokemon[i].wildHeldItems = wildHeldItems
+            self.pokemon[i].wildHeldItems = processData(byteSeq[0xb:0xd])
+
             # *** Gender ratio ***
-            genderRatio = ord(byteSeq[0xd])
-            self.pokemon[i].genderRatio = genderRatio
+            self.pokemon[i].genderRatio = processData(byteSeq[0xd])
+
             # *** Egg cycles ***
-            eggCycles = ord(byteSeq[0xf])
-            self.pokemon[i].eggCycles = eggCycles
+            self.pokemon[i].eggCycles = processData(byteSeq[0xf])
+
             # *** Dimensions ***
-            dimensions = ord(byteSeq[0x11])
-            self.pokemon[i].dimensions = dimensions
+            self.pokemon[i].dimensions = processData(byteSeq[0x11])
+
             # *** Growth Rate ***
-            growthRate = ord(byteSeq[0x16])
-            self.pokemon[i].growthRate = growthRate
+            self.pokemon[i].growthRate = processData(byteSeq[0x16])
+
             # *** Egg groups ***
-            # had no idea right now how to extract it 'nicely'
-            # because 2 egg groups are contained in 1 byte
-            eggGroups = ord(byteSeq[0x17])
-            eggGroups = map(hexToDec, '%x' % eggGroups)
-            self.pokemon[i].eggGroups = eggGroups
+            self.pokemon[i].eggGroups = processEggGroup(byteSeq[0x17])
+
             # *** TM/HM flags ***
-            # this probably is ugly but works...
-            # converts every byte into string of 0-1's
-            # then makes it a list of 8 0-1 ints.
-            tms = [0] * 64
-            for b, byte in enumerate(byteSeq[0x18:0x20]):
-                byte = ord(byte)
-                # bin(int) returns string starting with '0b'
-                # tms are put in reversed order, eg. (8, 7, ..., 1)
-                # '0b' is cut out and string reversed with [:1:-1]
-                byte = bin(byte)[:1:-1]
-                byte = map(int, byte)
-                tms[b*8:8 + b*8] = byte
-            self.pokemon[i].tms = tms[:]
+            self.pokemon[i].tms = processTms(byteSeq[0x18:0x20])

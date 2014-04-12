ba8f0ebe3926f1a1cfb9e2161099e96811dfc21f
Akwa <pokemonfakeemall@gmail.com>
2014-04-12 13:35:09 +0200

    changed design of Pokemon class

--- fakeemall/pokemonClass.py
+++ fakeemall/pokemonClass.py
@@ -4,22 +4,32 @@ from constants import *
 import random
 
 class Pokemon(object):
+    pass
 
+class PokemonContainer(object):
+
+    def __init__(self):
+        self.pokemon = {i: Pokemon() for i in xrange(maxPokemon)}
+
+    def __getitem__(self, item):
+        return self.pokemon[item]
+    
     def extractNames(self, data):
         """
         From the flat name sequence, extracts 10 bytes per iteration.
         Translates from bytecode to normal letters and finally,
         creates a dict with Pokémon names.
         """
-        lenData = len(data)
-        self.names = {}
-        # lenData should be 2510, lenName is 10
-        # so this xrange looks like (0, 10, 20, ..., 2500)
-        for i in xrange(0, lenData, lenName):
-            # charSeq is the sequence of 10 bytes
-            byteSeq = data[i:i + lenName]
+        null = '\x50'
+        for i in xrange(maxPokemon):
+            j = i * lenName
+            # byteSeq is the sequence of 10 bytes
+            byteSeq = data[j:j + lenName]
             # translate every byte of our sequence if possible
             # then, ''.join() this list of translated chars
-            name = ''.join([alph[b] for b in byteSeq if b != '\x50'])
-            # Add a new entry to our name dict
-            self.names[len(self.names)] = name
+            name = ''.join([alph[b] for b in byteSeq if b != null])
+            # Assign the name
+            self.pokemon[i].name = name
+
+            
+        
